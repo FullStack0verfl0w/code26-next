da3adb1cb8f6110f2d8f4ae82e7e0e79e7f7e554
FullStack0verfl0w <pBinarySoul@gmail.com>
2022-02-21 12:20:02 +0300

    Added 404 page

---
+++ pages/404/index.tsx
@@ -0,0 +1,23 @@
+import { FC } from "react";
+import Link from "next/link";
+import Sad from "../../public/sad-face.svg";
+import styles from "./404.module.css";
+
+export type NotFoundProps = { };
+
+const NotFound: FC<NotFoundProps> = (props: NotFoundProps) => {
+    return (
+        <main className={styles.mainContainer}>
+            <div className={styles.sadFace}>
+                <Sad />
+            </div>
+            <div className={styles.errorContainer}>
+                <span className={styles.errorTextBold}>404</span>
+                <span className={styles.errorText}>Sorry, but this page doesn&apos;t exist.</span>
+                <Link href="/"><span className={styles.backButton}>Back to Home</span></Link>
+            </div>
+        </main>
+    );
+};
+
+export default NotFound;
